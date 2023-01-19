ca439400840107f54e7cb22f723e3fe61f2e9eb7
Wo Yong Jeong <woyong.jeong@gmail.com>
2023-01-18 23:28:05 -0500

    Pair Programming with Yousef

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,17 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const transposeJoin = transpose(letters).map(ls => ls.join(''));
+  
+    for (l of horizontalJoin || transposeJoin) {
+      if (l.includes(word)) return true;
     }
-}
+  
+    return false;
+};
+  
+  
+const transpose = function(letters) {
+    return letters[0].map((col, i) => letters.map(row => row[i]));
+};
 
 module.exports = wordSearch
